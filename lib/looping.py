adecae7ddf1c27e3bf03b91b9f04743f3a3d32f5
vincent <vinnymummo@gmail.com>
2024-05-24 19:43:33 +0300

    commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
     # code goes here!
     pass
 
 def square_integers(int_list):
+    square_integers = [int * int for int in int_list]
+    return square_integers
     # code goes here!
     pass
 
 def fizzbuzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     # code goes here!
     pass
